00df51c3114fa0ab54060671e99168a8ab538fe9
Arthur99Silva <arthurassilva99@gmail.com>
2024-04-22 22:53:54 -0300

    Update PoseEstimationMin.py

--- PoseEstimationMin.py
+++ PoseEstimationMin.py
@@ -2,12 +2,21 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture('PoseVideos/yoga2.mp4')
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture('PoseVideos/run.mp4')
 pTime = 0
 
 while True:
     success, img = cap.read()
-    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks)
+
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
