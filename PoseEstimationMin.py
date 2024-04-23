87dc2492d76d8dc38d4e2cc148cdd09c38c0135c
Arthur99Silva <arthurassilva99@gmail.com>
2024-04-23 13:42:16 -0300

    Update PoseEstimationMin.py

--- PoseEstimationMin.py
+++ PoseEstimationMin.py
@@ -6,7 +6,7 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture('PoseVideos/run.mp4')
+cap = cv2.VideoCapture('PoseVideos/yoga2.mp4')
 pTime = 0
 
 while True:
@@ -15,7 +15,12 @@ while True:
     results = pose.process(imgRGB)
     print(results.pose_landmarks)
     if results.pose_landmarks:
-        mpDraw.draw_landmarks(img, results.pose_landmarks)
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            cv2.circle(img, (cx, cy), 5, (255, 0 , 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
